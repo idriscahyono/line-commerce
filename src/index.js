ac1fd4ee6a3825bc33a271a828e8cf38f1eefa7d
idriscahyonoo <idriscahyono@gmail.com>
2019-11-12 15:34:11 +0700

    first

---
+++ src/index.js
@@ -0,0 +1,47 @@
+'use strict'
+
+const line = require('@line/bot-sdk');
+const express = require('express');
+
+
+const defaultAccessToken = 'Upq28CGaamhChqJ9l/+F1EU3TccKCzsTMhbRP7/EqXch4nQgqsALQtdUmwA0h0DcwMk79lXWBxui3A8keQXADD1IT7XWuLF1QnEzjYwhADZu7CspRiNfJ7LWxcViJCXIbytMSNqfAkGO4U1mIJkG1gdB04t89/1O/w1cDnyilFU=';
+const defaultSecret = 'c9c27ba18352f53ef13f2430c3d7b9db';
+
+const config = {
+    channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN || defaultAccessToken,
+    channelSecret: process.env.CHANNEL_SECRET || defaultSecret,
+};
+
+const client = new line.Client(config);
+const app = express();
+
+app.get("/", function (req, res) {
+    res.send("Hello Word");
+});
+
+app.post("/webhook", line.middleware(config), function (req, res) {
+    Promise
+        .all(req.body.events.map(handleEvent))
+        .then((result) => res.json(result))
+        .catch((err) => {
+            console.error(err);
+            res.status(500).end();
+        });
+});
+
+function handleEvent(event) {
+    if (event.type !== 'message' || event.message.type !== 'text') {
+        return Promise.resolve(null);
+    }
+    const echo = {
+        type: 'text',
+        text: event.message.text
+    };
+
+    return client.replyMessage(event.replyToken, echo);
+}
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`listening on ${port}`);
+});

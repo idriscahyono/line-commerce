e7e6f3bc2c739a38a22888bce84ec9928b898004
idriscahyonoo <idriscahyono@gmail.com>
2019-11-13 21:52:38 +0700

    test 15

--- src/index.js
+++ src/index.js
@@ -166,207 +166,72 @@ function handleText(message, replyToken, source) {
                     replyToken, {
                         type: "carousel",
                         contents: [{
-                                type: "bubble",
-                                hero: {
-                                    type: "image",
-                                    size: "full",
-                                    aspectRatio: "20:13",
-                                    aspectMode: "cover",
-                                    url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_5_carousel.png"
-                                },
-                                body: {
-                                    type: "box",
-                                    layout: "vertical",
-                                    spacing: "sm",
-                                    contents: [{
-                                            type: "text",
-                                            text: "Arm Chair, White",
-                                            wrap: true,
-                                            weight: "bold",
-                                            size: "xl"
-                                        },
-                                        {
-                                            type: "box",
-                                            layout: "baseline",
-                                            contents: [{
-                                                    type: "text",
-                                                    text: "$49",
-                                                    wrap: true,
-                                                    weight: "bold",
-                                                    size: "xl",
-                                                    flex: 0
-                                                },
-                                                {
-                                                    type: "text",
-                                                    text: ".99",
-                                                    wrap: true,
-                                                    weight: "bold",
-                                                    size: "sm",
-                                                    flex: 0
-                                                }
-                                            ]
-                                        }
-                                    ]
-                                },
-                                footer: {
-                                    type: "box",
-                                    layout: "vertical",
-                                    spacing: "sm",
-                                    contents: [{
-                                            type: "button",
-                                            style: "primary",
-                                            action: {
-                                                type: "uri",
-                                                label: "Add to Cart",
-                                                uri: "https://linecorp.com"
-                                            }
-                                        },
-                                        {
-                                            type: "button",
-                                            action: {
-                                                type: "uri",
-                                                label: "Add to wishlist",
-                                                uri: "https://linecorp.com"
-                                            }
-                                        }
-                                    ]
-                                }
+                            type: "bubble",
+                            hero: {
+                                type: "image",
+                                size: "full",
+                                aspectRatio: "20:13",
+                                aspectMode: "cover",
+                                url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_5_carousel.png"
                             },
-                            {
-                                type: "bubble",
-                                hero: {
-                                    type: "image",
-                                    size: "full",
-                                    aspectRatio: "20:13",
-                                    aspectMode: "cover",
-                                    url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_5_carousel.png"
-                                },
-                                body: {
-                                    type: "box",
-                                    layout: "vertical",
-                                    spacing: "sm",
-                                    contents: [{
-                                            type: "text",
-                                            text: "Arm Chair, White",
-                                            wrap: true,
-                                            weight: "bold",
-                                            size: "xl"
-                                        },
-                                        {
-                                            type: "box",
-                                            layout: "baseline",
-                                            contents: [{
-                                                    type: "text",
-                                                    text: "$49",
-                                                    wrap: true,
-                                                    weight: "bold",
-                                                    size: "xl",
-                                                    flex: 0
-                                                },
-                                                {
-                                                    type: "text",
-                                                    text: ".99",
-                                                    wrap: true,
-                                                    weight: "bold",
-                                                    size: "sm",
-                                                    flex: 0
-                                                }
-                                            ]
-                                        }
-                                    ]
-                                },
-                                footer: {
-                                    type: "box",
-                                    layout: "vertical",
-                                    spacing: "sm",
-                                    contents: [{
-                                            type: "button",
-                                            style: "primary",
-                                            action: {
-                                                type: "uri",
-                                                label: "Add to Cart",
-                                                uri: "https://linecorp.com"
+                            body: {
+                                type: "box",
+                                layout: "vertical",
+                                spacing: "sm",
+                                contents: [{
+                                        type: "text",
+                                        text: "Arm Chair, White",
+                                        wrap: true,
+                                        weight: "bold",
+                                        size: "xl"
+                                    },
+                                    {
+                                        type: "box",
+                                        layout: "baseline",
+                                        contents: [{
+                                                type: "text",
+                                                text: "$49",
+                                                wrap: true,
+                                                weight: "bold",
+                                                size: "xl",
+                                                flex: 0
+                                            },
+                                            {
+                                                type: "text",
+                                                text: ".99",
+                                                wrap: true,
+                                                weight: "bold",
+                                                size: "sm",
+                                                flex: 0
                                             }
-                                        },
-                                        {
-                                            type: "button",
-                                            action: {
-                                                type: "uri",
-                                                label: "Add to wishlist",
-                                                uri: "https://linecorp.com"
-                                            }
-                                        }
-                                    ]
-                                }
+                                        ]
+                                    }
+                                ]
                             },
-                            {
-                                type: "bubble",
-                                hero: {
-                                    type: "image",
-                                    size: "full",
-                                    aspectRatio: "20:13",
-                                    aspectMode: "cover",
-                                    url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_5_carousel.png"
-                                },
-                                body: {
-                                    type: "box",
-                                    layout: "vertical",
-                                    spacing: "sm",
-                                    contents: [{
-                                            type: "text",
-                                            text: "Arm Chair, White",
-                                            wrap: true,
-                                            weight: "bold",
-                                            size: "xl"
-                                        },
-                                        {
-                                            type: "box",
-                                            layout: "baseline",
-                                            contents: [{
-                                                    type: "text",
-                                                    text: "$49",
-                                                    wrap: true,
-                                                    weight: "bold",
-                                                    size: "xl",
-                                                    flex: 0
-                                                },
-                                                {
-                                                    type: "text",
-                                                    text: ".99",
-                                                    wrap: true,
-                                                    weight: "bold",
-                                                    size: "sm",
-                                                    flex: 0
-                                                }
-                                            ]
+                            footer: {
+                                type: "box",
+                                layout: "vertical",
+                                spacing: "sm",
+                                contents: [{
+                                        type: "button",
+                                        style: "primary",
+                                        action: {
+                                            type: "uri",
+                                            label: "Add to Cart",
+                                            uri: "https://linecorp.com"
                                         }
-                                    ]
-                                },
-                                footer: {
-                                    type: "box",
-                                    layout: "vertical",
-                                    spacing: "sm",
-                                    contents: [{
-                                            type: "button",
-                                            style: "primary",
-                                            action: {
-                                                type: "uri",
-                                                label: "Add to Cart",
-                                                uri: "https://linecorp.com"
-                                            }
-                                        },
-                                        {
-                                            type: "button",
-                                            action: {
-                                                type: "uri",
-                                                label: "Add to wishlist",
-                                                uri: "https://linecorp.com"
-                                            }
+                                    },
+                                    {
+                                        type: "button",
+                                        action: {
+                                            type: "uri",
+                                            label: "Add to wishlist",
+                                            uri: "https://linecorp.com"
                                         }
-                                    ]
-                                }
+                                    }
+                                ]
                             }
-                        ]
+                        }]
                     }
 
                 )

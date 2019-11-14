ee637c1d3398a98d330ad5042f0e761b1c7b0525
idriscahyonoo <idriscahyono@gmail.com>
2019-11-14 22:32:22 +0700

    test39

--- src/index.js
+++ src/index.js
@@ -74,47 +74,46 @@
                         case 'BUY':
                             return client.replyMessage(
                                 replyToken, {
-                                    // type: 'template',
-                                    // altText: 'Recipe',
-                                    // template: {
-                                    //     type: 'confirm',
-                                    //     text: 'Do it?',
-                                    //     actions: [{
-                                    //             label: 'Yes',
-                                    //             type: 'message',
-                                    //             text: 'Yes!'
-                                    //         },
-                                    //         {
-                                    //             label: 'No',
-                                    //             type: 'message',
-                                    //             text: 'No!'
-                                    //         },
-                                    //     ],
-                                    // },
-
-                                    type: 'flex',
-                                    allText: 'Recipe',
-                                    flex: {
-                                        type: 'flex',
-                                        altText: 'this is a flex message',
-                                        contents: {
-                                            type: 'bubble',
-                                            body: {
-                                                type: 'box',
-                                                layout: 'vertical',
-                                                contents: [{
-                                                        type: 'text',
-                                                        text: 'hello'
-                                                    },
-                                                    {
-                                                        type: 'text',
-                                                        text: 'world'
-                                                    }
-                                                ]
-                                            }
-                                        }
-                                    }
-                                },
+                                    type: 'template',
+                                    altText: 'Recipe',
+                                    template: {
+                                        type: 'confirm',
+                                        text: 'Do it?',
+                                        actions: [{
+                                                label: 'Yes',
+                                                type: 'message',
+                                                text: 'Yes!'
+                                            },
+                                            {
+                                                label: 'No',
+                                                type: 'message',
+                                                text: 'No!'
+                                            },
+                                        ],
+                                    },
+                                }
+                                // type: 'flex',
+                                // allText: 'Recipe',
+                                // flex: {
+                                //     type: 'flex',
+                                //     altText: 'this is a flex message',
+                                //     contents: {
+                                //         type: 'bubble',
+                                //         body: {
+                                //             type: 'box',
+                                //             layout: 'vertical',
+                                //             contents: [{
+                                //                     type: 'text',
+                                //                     text: 'hello'
+                                //                 },
+                                //                 {
+                                //                     type: 'text',
+                                //                     text: 'world'
+                                //                 }
+                                //             ]
+                                //         }
+                                //     }
+                                // }
                             )
                     }
                     default:
